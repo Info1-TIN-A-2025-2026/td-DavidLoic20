6410ca9a7693aa2b796ccb54e8f211e9cefa2ee7
DavidLoic20 <loic.david@heig-vd.ch>
2025-09-16 12:32:46 +0000

    added TD20250916

---
+++ TD20250916/td.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	unsigned int num_students = 23; // nombre entier non signé 
+	int account_value = -42; // nombre entier signé
+	float temperature = 18.1; //32 bits
+	double pi = 3.141592; //64 bits
+}
